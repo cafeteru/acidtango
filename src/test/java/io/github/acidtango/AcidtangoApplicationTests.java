cf818f304185235b102d3ec14204cc07dcb40cce
Iván González <cafeteru.dev@gmail.com>
2023-05-28 22:03:26 +0200

    Create project

---
+++ src/test/java/io/github/acidtango/AcidtangoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.acidtango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcidtangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
