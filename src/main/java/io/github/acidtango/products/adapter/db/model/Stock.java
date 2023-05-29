1a35e05148aa1771d85f6da43bada275499859fa
Iván González <cafeteru.dev@gmail.com>
2023-05-30 00:26:25 +0200

    Delete unused imports

--- src/main/java/io/github/acidtango/products/adapter/db/model/Stock.java
+++ src/main/java/io/github/acidtango/products/adapter/db/model/Stock.java
@@ -1,6 +1,5 @@
 package io.github.acidtango.products.adapter.db.model;
 
-import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
--- src/test/java/io/github/acidtango/products/adapter/api/impl/ProductControllerTest.java
+++ src/test/java/io/github/acidtango/products/adapter/api/impl/ProductControllerTest.java
@@ -2,10 +2,8 @@ package io.github.acidtango.products.adapter.api.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
